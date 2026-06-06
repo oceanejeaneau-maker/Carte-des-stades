51328ea88794c6ddc87779dedded088f84e41e69
oceanejeaneau-maker <oceanejeaneau@gmail.com>
2026-06-06 11:19:12 +0200

    Update service worker to version 18

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 // ═══════════════════════════════════════════════
-// SERVICE WORKER — Groundhopper v17
+// SERVICE WORKER — Groundhopper v18
 // ═══════════════════════════════════════════════
 
-const CACHE_NAME = 'groundhopper-v17';
+const CACHE_NAME = 'groundhopper-v18';
 
 const PRECACHE_URLS = [
   '/manifest.json',
